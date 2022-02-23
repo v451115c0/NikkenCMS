468ab3ba2736a2a7e11aeedf3a30ef8741dd2ff3
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-02-23 15:23:29 -0600

    23/02/2022

--- app/Http/Controllers/NikkenCMS/NikkenCMSController.php
+++ app/Http/Controllers/NikkenCMS/NikkenCMSController.php
@@ -196,19 +196,19 @@ class NikkenCMSController extends Controller{
 
     public function contact(){
         $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+        $response = $client->request('PSOT', 'https://api.github.com/repos/guzzle/guzzle');
 
         echo $response->getStatusCode(); // 200
         echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
         echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
 
         // Send an asynchronous request.
-        $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+        /*$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
         $promise = $client->sendAsync($request)->then(function ($response) {
             echo 'I completed! ' . $response->getBody();
         });
 
-        $promise->wait();
+        $promise->wait();*/
 
         /*
         $response = Http::withHeaders([
