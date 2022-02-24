fcdb11151a0aa6245483a8d9df81da5de2e02a37
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-02-24 16:38:33 -0600

    24/02/2022

--- resources/views/NikkenCMS/businessToolsAdmin.blade.php
+++ resources/views/NikkenCMS/businessToolsAdmin.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <ol class="breadcrumb bg-primary">
-    <li class="breadcrumb-item"><a href="{{ url('NikkenCMS/home') }}" class="text-white"><i class="ri-home-4-line mr-1 float-left"></i>Home</a></li>
-    <li class="breadcrumb-item active text-white" aria-current="page">Herramientas de Negocio</li>
+    <li class="breadcrumitem"><a href="{{ url('NikkenCMS/home') }}" class="text-white"><i class="ri-home-4-line mr-1 float-left"></i>Home</a></li>
+    <li class="breadcrumitem active text-white" aria-current="page">Herramientas de Negocio</li>
 </ol>
 <div class="row">
     <div class="col-lg-12 col-md-12">
@@ -15,58 +15,58 @@
                 <div class="iq-card-body">
                     <div class="row">
                         <div class="col-lg-12 col-md-12">
-                            <b-table-simple responsive>
-                                <b-thead>
-                                  <b-th>#</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                  <b-th>Heading</b-th>
-                                </b-thead>
-                                <b-tbody>
-                                  <b-tr>
-                                    <b-th>1</b-th>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                  </b-tr>
-                                  <b-tr>
-                                    <b-th>2</b-th>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                  </b-tr>
-                                  <b-tr>
-                                    <b-th>3</b-th>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                    <b-td>Cell</b-td>
-                                  </b-tr>
-                                </b-tbody>
-                              </b-table-simple>
+                            <table class="table-simple" responsive>
+                                <thead>
+                                  <th>#</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                  <th>Heading</th>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <th>1</th>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                  </tr>
+                                  <tr>
+                                    <th>2</th>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                  </tr>
+                                  <tr>
+                                    <th>3</th>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                    <td>Cell</td>
+                                  </tr>
+                                </tbody>
+                              </table>
                         </div>
                     </div>
                 </div>
