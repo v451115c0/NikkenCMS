69c155592663787716d483bf36a6b5f742bfddc6
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-04-22 12:25:08 -0500

    22/04/2022

--- app/Http/Controllers/facturasCol/facturasColController.php
+++ app/Http/Controllers/facturasCol/facturasColController.php
@@ -60,8 +60,6 @@ class facturasColController extends Controller{
         $response = $client->request('POST', 'https://secfevalpruebas.ptesa.com.co:8443/api/fe/v1/integration/emission/company/100071/detail/documents/files', [
             'headers' => [
                 'Authorization' => 'Bearer ' . $access_token,
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
             ],
             'form_params' => [
                 "signature" => "d14025b5413060c5905d55e0e16062add8d5b9fe85400d17c69a0fd25fde0399a5d7bc1191bb670ffc8165b3c2318744",
@@ -77,7 +75,7 @@ class facturasColController extends Controller{
         return $response->getBody();
         $pdfb64 = json_decode($response->getBody());
 
-        return $pdfb64;
+        return $pdfb64->base64;
         return "hola";
     }
 }
