60a01f416c79297cb19e53050cbb74c3ab7e70e7
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-06-21 15:16:19 -0500

    21/06/2022

--- app/Http/Controllers/NikkenCMS/NikkenCMSController.php
+++ app/Http/Controllers/NikkenCMS/NikkenCMSController.php
@@ -575,7 +575,7 @@ class NikkenCMSController extends Controller{
         $file = "";
         $imagenes = $documento->getObjectsByType('XObject', 'Image');
         foreach ($imagenes as $imagen) {
-            $file .= '<img src="data:image/jpg;base64,'. $imagen->getContent() .'" />';
+            $file .= '<img src="data:image/jpg;base64,'. base64_encode($imagen->getContent()) .'" />';
         }
         
         return $file;
