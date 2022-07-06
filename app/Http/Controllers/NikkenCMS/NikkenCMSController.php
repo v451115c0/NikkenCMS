f0cdfe3afb26acd05eba889c8857861e3325061f
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-07-06 15:42:31 -0500

    06/07/2022

--- app/Http/Controllers/NikkenCMS/NikkenCMSController.php
+++ app/Http/Controllers/NikkenCMS/NikkenCMSController.php
@@ -746,7 +746,7 @@ class NikkenCMSController extends Controller{
             }
             ($RFC === $this->delete_space($data2['pdfUSER']['RFC'], '')) ? $RFC = "valido": $RFC = 'invalido';
 
-            return $textGral;
+            return $data2['pdfUSER'];
             $table = '<table border="1px" width="100%">' .
                         '<thead>' .
                             '<tr>' .
