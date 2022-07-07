90541631971710020e1e5b3664482b70d76e37e2
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-07-07 18:27:30 -0500

    07/07/2022

--- app/Http/Controllers/NikkenCMS/NikkenCMSController.php
+++ app/Http/Controllers/NikkenCMS/NikkenCMSController.php
@@ -596,7 +596,7 @@ class NikkenCMSController extends Controller{
             $insert = "INSERT INTO users_fiscal_update(user_id,sap_code,rfc,person_type,regimen_code,regimen_description,business_name,name,last_name,second_last_name,cp,estado,municipio,colonia,cfdi_code,cfdi_description,fiscal_file,comments,updated_on_sql_server,existeSap,created_at,updated_at)
             VALUES ('1954', '14829503', '" . strtoupper($data2['pdfUSER']['RFC']) . "', '" . $data2['pdfUSER']['tipo'] . "', '" . $data2['pdfUSER']['regimen'] . "', '" . strtoupper($data2['pdfUSER']['regimenDescriptor']) . "', '', '" . strtoupper($data2['pdfUSER']['nombre']) . "', '" . strtoupper($data2['pdfUSER']['apellido1']) . "', '" . strtoupper($data2['pdfUSER']['apellido2']) . "', '" . $data2['pdfUSER']['cp'] . "', 'ESTADO DE MÉXICO', 'NICOLÁS ROMERO', 'BENITO JUÁREZ 1A. SECCIÓN (CABECERA MUNICIPAL)', 'S01', 'SIN EFECTOS FISCALES', 'https://storage.googleapis.com/tv-store/datos-fiscales/1656438198_XAXX010101000_correcto.pdf', '', '0', '0', '2022-07-06 15:26:15', '2022-07-06 15:26:15')";
             
-            return $insert;
+            //return $insert;
             
             $conexion = \DB::connection('migracion');
                 $response = $conexion->insert("$insert");
