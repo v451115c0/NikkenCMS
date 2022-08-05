a9ea35e44cdbf90213e01112fca6260ce478a0ca
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-08-05 13:08:48 -0500

    05/08/2022

--- app/Http/Controllers/NikkenCMS/NikkenCMSController.php
+++ app/Http/Controllers/NikkenCMS/NikkenCMSController.php
@@ -757,7 +757,6 @@ class NikkenCMSController extends Controller{
                     $search_term = "Regímenes";
                     $position = $this->search_array($textGral, $search_term);
                     $position = (intval($position) + 2);
-                    return $position;
                     $data['regimenDescriptor'] = trim($this->deleteNumbersSepecialChar($this->delete_space($textGral[$position], ' '), ''));
                     $data['regimen'] = $arrayRegimenCode[trim($data['regimenDescriptor'])];
                     return $data;
