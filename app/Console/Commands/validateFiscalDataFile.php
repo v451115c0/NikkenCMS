d5941a60ac625347c9ed0286cbca4dd8121da0b7
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-08-30 12:14:18 -0500

    Update validateFiscalDataFile.php

--- app/Console/Commands/validateFiscalDataFile.php
+++ app/Console/Commands/validateFiscalDataFile.php
@@ -180,7 +180,7 @@ class validateFiscalDataFile extends Command
                 $data2['pdfUSER'] = $data;
 
                 ## se procesa el archivo PDF generado a partir del QR en el archivo que adjunta el usuario desde la TV
-                ConvertApi::setApiSecret('x73XwF7GsGyGeK1q');
+                ConvertApi::setApiSecret('784SYWV5QCTzNz1K');
                 $result = ConvertApi::convert('jpg', [
                         'File' => "$PDFfile",
                         'PageRange' => '1-1',
