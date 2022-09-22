5f26a91bbb318e70adedfee8547cf71d03874db5
Fmelchor Nikken <54543540+v451115c0@users.noreply.github.com>
2022-09-22 18:32:23 -0500

    Update NikkenCMSController.php

--- app/Http/Controllers/NikkenCMS/NikkenCMSController.php
+++ app/Http/Controllers/NikkenCMS/NikkenCMSController.php
@@ -370,7 +370,7 @@ class NikkenCMSController extends Controller{
                 //$pdf = $parser->parseFile($PDFfile);
                 $data = [];
                 $textGral = $pdf->getText();
-                return $textGral;
+                #return $textGral;
 
                 /*$find = "CÉDULA DE IDENTIFICACIÓN FISCAL";
                 $validaTexto = strpos($textGral, $find);*/
